4e23cb9fe9f74371fbdf2e066c94afc8e4951f69
Shikhar Mandloi <shikhar.mandloi@gmail.com>
2024-03-20 01:07:57 +0530

    feat: router initialized

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       NotOmegle
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
